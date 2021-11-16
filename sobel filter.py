1eb99727d02a98b8472db84a4aa51c2199b53ac8
mrmezan06 <mrmezan06@gmail.com>
2021-11-17 01:12:38 +0600

    Applying another combining method on sobelxy

--- sobel filter.py
+++ sobel filter.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Read the original image
 img = cv2.imread('test.jpg') 
@@ -15,12 +16,15 @@ img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
 sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
 sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
 sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+# another Combining method
+sblxy = np.sqrt(sobelx**2 + sobely**2)
 # Display Sobel Edge Detection Images
 cv2.imshow('Sobel X', sobelx)
 cv2.waitKey(0)
 cv2.imshow('Sobel Y', sobely)
 cv2.waitKey(0)
 cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.imshow('Sobel XY combining with normalization:', sblxy)
 cv2.waitKey(0)
 
 # Canny Edge Detection
